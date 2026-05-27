4ec951b94ba5b64b06e733df9f0b44af024c4196
Pooja Aganathan <pooja110606@gmail.com>
2026-05-27 10:57:53 +0530

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,1 +1,24 @@
+def create_features(df):
 
+    # BMI Risk Category
+
+    if 'bmi' in df.columns:
+
+        df['bmi_category'] = df['bmi'].apply(
+
+            lambda x:
+
+            'Underweight'
+            if x < 18.5
+
+            else 'Normal'
+            if x < 25
+
+            else 'Overweight'
+            if x < 30
+
+            else 'Obese'
+
+        )
+
+    return df
