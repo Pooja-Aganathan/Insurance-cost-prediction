89283c1e3a4b04c33c9ea03a3c474ae0bfdcdc2c
Pooja Aganathan <pooja110606@gmail.com>
2026-05-27 10:57:35 +0530

    Update data_preprocessing.py

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,1 +1,39 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
 
+
+def remove_duplicates(df):
+
+    df = df.drop_duplicates()
+
+    return df
+
+
+def encode_features(df):
+
+    encoder = LabelEncoder()
+
+    categorical_cols = [
+        'sex',
+        'smoker',
+        'region'
+    ]
+
+    for col in categorical_cols:
+
+        if col in df.columns:
+
+            df[col] = encoder.fit_transform(
+                df[col]
+            )
+
+    return df
+
+
+def scale_features(X):
+
+    scaler = StandardScaler()
+
+    X_scaled = scaler.fit_transform(X)
+
+    return X_scaled
