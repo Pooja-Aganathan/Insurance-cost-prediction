2ca2183aaf337719d919d6f75f52e1df6bcae053
Pooja Aganathan <pooja110606@gmail.com>
2026-05-27 10:58:16 +0530

    Update model_training.py

--- src/model_training.py
+++ src/model_training.py
@@ -1,1 +1,46 @@
+from sklearn.linear_model import LinearRegression
 
+from sklearn.ensemble import RandomForestRegressor
+
+from xgboost import XGBRegressor
+
+from lightgbm import LGBMRegressor
+
+
+def train_models(
+    X_train,
+    y_train
+):
+
+    models = {}
+
+    models[
+        'Linear Regression'
+    ] = LinearRegression()
+
+    models[
+        'Random Forest'
+    ] = RandomForestRegressor(
+        random_state=42
+    )
+
+    models[
+        'XGBoost'
+    ] = XGBRegressor(
+        random_state=42
+    )
+
+    models[
+        'LightGBM'
+    ] = LGBMRegressor(
+        random_state=42
+    )
+
+    for name, model in models.items():
+
+        model.fit(
+            X_train,
+            y_train
+        )
+
+    return models
